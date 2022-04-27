6fc6834690aa34cce0ceef9ad57daea620534dd7
Saidamir <69676883+Saidamir-Sh@users.noreply.github.com>
2022-04-27 14:34:02 +0200

    theming Mui

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,38 @@
 import React from 'react';
 import './App.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid'
+import Item from '@mui/material/ListItem'
+import NavbarComponent from './Components/NavbarComponent'
 
 function App() {
   return (
-    <div className="App">
-    
-    </div>
+    <Container style={{
+      border: "solid",
+      minWidth: "100%",
+      height: "100vh",
+      padding: '0%'
+    }}>
+      <BrowserRouter>
+        <Grid container spacing={0}>
+          <Grid item xs={12}>
+            <Routes>
+              <Route path='/' element={<NavbarComponent />}/>
+            </Routes>
+          </Grid>
+          <Grid item xs={4}>
+            <Item>xs=4</Item>
+          </Grid>
+          <Grid item xs={4}>
+            <Item>xs=4</Item>
+          </Grid>
+          <Grid item xs={8}>
+            <Item>xs=8</Item>
+          </Grid>
+        </Grid>
+      </BrowserRouter>
+    </Container>
   );
 }
 
