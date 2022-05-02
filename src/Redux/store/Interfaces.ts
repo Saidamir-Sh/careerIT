ff81dfe49155529400d794b10280d44a0ffd54f8
Saidamir <69676883+Saidamir-Sh@users.noreply.github.com>
2022-05-02 03:39:05 +0200

    job interface

---
+++ src/Redux/store/Interfaces.ts
@@ -0,0 +1,12 @@
+export interface Job {
+    _id:                         string;
+    url:                         string;
+    title:                       string;
+    company_name:                string;
+    category:                    string;
+    job_type:                    string;
+    publication_date:            Date;
+    candidate_required_location: string;
+    salary:                      string;
+    description:                 string;
+}
