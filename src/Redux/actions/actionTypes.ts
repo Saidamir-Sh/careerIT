4ac4b03dabd99ca3d6a919dd77a961045e5ab7fe
Saidamir <69676883+Saidamir-Sh@users.noreply.github.com>
2022-05-02 02:45:22 +0200

    action interface

--- src/Redux/actions/actionTypes.ts
+++ src/Redux/actions/actionTypes.ts
@@ -6,3 +6,31 @@ export enum ActionType {
     ADD_TO_SAVED = 'ADD_TO_SAVED',
     REMOVE_FROM_SAVED = 'REMOVE_FROM_SAVED'
 }
+
+interface actionFetchJobs {
+    type: ActionType.FETCH_JOBS;
+}
+
+interface actionSearch {
+    type: ActionType.FETCH_SEARCHED_JOBS;
+    payload: Comment[];
+}
+
+interface actionByCategory {
+    type: ActionType.FETCH_BY_CATEGORY;
+    payload: string;
+}
+interface actionFetchCompany {
+    type: ActionType.FETCH_COMPANY;
+    payload: string;
+}
+interface actionAddSaved {
+    type: ActionType.ADD_TO_SAVED;
+    payload: string;
+}
+interface actionRemoveSaved {
+    type: ActionType.REMOVE_FROM_SAVED;
+    payload: string;
+}
+
+export type Action = actionFetchJobs | actionSearch | actionByCategory | actionByCategory | actionFetchCompany | actionAddSaved | actionRemoveSaved;
