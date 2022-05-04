282e441217ce19ee0407d094a162ffcb0884b683
Saidamir <69676883+Saidamir-Sh@users.noreply.github.com>
2022-05-04 07:07:23 +0200

    detail component styled

--- src/Components/DetailsComponent.tsx
+++ src/Components/DetailsComponent.tsx
@@ -1,13 +1,22 @@
+import { Typography } from '@mui/material'
+import { Box } from '@mui/system'
+import { Interweave } from 'interweave'
 import React from 'react'
 import { useParams } from 'react-router'
+import { useTypedSelector } from '../Redux/hooks/useTypeSelector'
 
 function DetailsComponent() {
 
     const params = useParams()
     const jobId = params.job_id
-    console.log(jobId)
+    const jobs = useTypedSelector((state) => state.jobs)
+    const selectedJob = jobs.find((job) => job._id === jobId)
+    console.log(selectedJob)
   return (
-    <div>DetailsComponent</div>
+    <Box component='div'>
+        <Typography></Typography>
+        <Interweave content={selectedJob?.description}/>
+    </Box>
   )
 }
 
--- src/Pages/JobDetails.tsx
+++ src/Pages/JobDetails.tsx
@@ -4,6 +4,16 @@ import DetailsComponent from '../Components/DetailsComponent'
 import NavbarComponent from '../Components/NavbarComponent'
 
 function JobDetails() {
+
+    const detailsStyle = {
+        position: 'absolute', 
+        width: '80%', 
+        bottom: 0, 
+        left: '50%', 
+        transform: 'translate(-50%, 0%)', 
+        height: '50vh', 
+        overflowY: 'scroll'
+    }
   return (
     <Container style={{
         minWidth: "100%",
@@ -11,8 +21,8 @@ function JobDetails() {
         padding: '0%'
       }}>
         <NavbarComponent />
-        <Grid container>
-            <Grid item>
+        <Grid display='flex'>
+            <Grid item xs={8}  sx={detailsStyle}>
                 <DetailsComponent />
             </Grid>
         </Grid>
